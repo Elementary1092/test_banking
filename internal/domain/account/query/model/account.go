dac7323face104c607ca25377523bfafca3612d5
Elementary1092 <pdead6223@gmail.com>
2023-04-12 21:53:30 +0500

    [WIP] Redifine account read model

--- internal/domain/account/query/model/account.go
+++ internal/domain/account/query/model/account.go
@@ -3,38 +3,11 @@ package model
 import "github.com/Elementary1092/test_banking/internal/entity"
 
 type Account struct {
-    Info *entity.Account
+	Info *entity.Account
 
-    // account should belong to some registered user
-    UserID string
+	// account should belong to some registered user
+	UserID string
 
-    // account may have many outgoing transactions
-    outTransactions []*entity.Transaction
-
-    // account may be addressed by many transactions
-    inTransactions []*entity.Transaction
-}
-
-func (a *Account) OutTransactions() []*entity.Transaction {
-    return a.outTransactions
-}
-
-func (a *Account) AddOutTransactions(transactions ...*entity.Transaction) {
-    if len(transactions) == 0 {
-        return
-    }
-
-    a.outTransactions = append(a.outTransactions, transactions...)
-}
-
-func (a *Account) InTransactions() []*entity.Transaction {
-    return a.inTransactions
-}
-
-func (a *Account) AddInTransactions(transactions ...*entity.Transaction) {
-    if len(transactions) == 0 {
-        return
-    }
-
-    a.outTransactions = append(a.outTransactions, transactions...)
+	// account may have many outgoing transactions
+	Transactions []*entity.Transaction
 }
