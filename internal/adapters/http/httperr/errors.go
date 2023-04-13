55340ff38585579112188f6d0e89b03f04600553
Elementary1092 <pdead6223@gmail.com>
2023-04-14 04:13:47 +0500

    [FIX] Error responses

--- internal/adapters/http/httperr/errors.go
+++ internal/adapters/http/httperr/errors.go
@@ -1,6 +1,9 @@
 package httperr
 
 import (
+	"errors"
+	"github.com/Elementary1092/test_banking/internal/adapters/dao"
+	accCmdErr "github.com/Elementary1092/test_banking/internal/domain/account/command/errResponses"
 	"github.com/json-iterator/go"
 	"net/http"
 )
@@ -12,14 +15,42 @@ type ErrorResponse struct {
 	Status int    `json:"status"`
 }
 
+// WrapError wraps errors from command, queries and DAO
 func WrapError(w http.ResponseWriter, err error) {
 	resp := ErrorResponse{
-		Slug:   err.Error(),
 		Status: http.StatusBadRequest,
 	}
 
+	// Not the best decision, unique error codes could have better
+	if errors.Is(err, accCmdErr.ErrInsufficientFunds) {
+		resp.Slug = "insufficient-balance"
+		resp.Status = http.StatusBadRequest
+	} else if errors.Is(err, accCmdErr.ErrInvalidTransactionAmount) {
+		resp.Slug = "invalid-amount"
+	} else if errors.Is(err, accCmdErr.ErrInvalidAccountInfo) {
+		resp.Slug = "invalid-account-info"
+	} else if errors.Is(err, accCmdErr.ErrFailedToGenAccNumber) {
+		resp.Status = http.StatusInternalServerError
+		resp.Slug = "account-number-error"
+	} else if errors.Is(err, accCmdErr.ErrInvalidRecipientCurrency) {
+		resp.Slug = "currencies-do-not-match"
+	} else if errors.Is(err, accCmdErr.ErrInvalidUpdateParameters) {
+		resp.Slug = "invalid-account-data"
+	} else if errors.Is(err, accCmdErr.ErrInvalidBalanceInfo) {
+		resp.Slug = "invalid-balance"
+	} else if errors.Is(err, dao.ErrDuplicate{}) {
+		resp.Slug = "duplicate-data"
+	} else if errors.Is(err, dao.ErrInvalidEmpty{}) {
+		resp.Slug = "missing-required-data"
+	} else if errors.Is(err, dao.ErrNotFound{}) {
+		resp.Slug = "not-found"
+		resp.Status = http.StatusNotFound
+	} else {
+		resp.Slug = "internal-error"
+	}
+
 	encoder := json.NewEncoder(w)
-	w.WriteHeader(http.StatusBadRequest)
+	w.WriteHeader(resp.Status)
 	encoder.Encode(resp)
 }
 
