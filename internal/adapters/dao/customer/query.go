935e27ff11394e984d459e98b890a06eab4c9b28
Elementary1092 <pdead6223@gmail.com>
2023-04-13 13:54:26 +0500

    [WIP] Implement customer query and command dao

--- internal/adapters/dao/customer/query.go
+++ internal/adapters/dao/customer/query.go
@@ -1,1 +1,62 @@
 package customer
+
+import (
+	"context"
+	"fmt"
+	"github.com/Elementary1092/test_banking/internal/adapters/dao"
+	"github.com/Elementary1092/test_banking/internal/domain/customer/query/model"
+	"strings"
+)
+
+type QueryDAO struct {
+	db                dao.DB
+	customerTableName string
+}
+
+func NewQueryDAO(db dao.DB) *QueryDAO {
+	if db == nil {
+		panic("db is nil in dao/customer.QueryDAO")
+	}
+
+	return &QueryDAO{
+		db:                db,
+		customerTableName: `"customers"`,
+	}
+}
+
+func (q *QueryDAO) FindCustomer(ctx context.Context, filter map[string]string) (*model.Customer, error) {
+	const queryFmt = `SELECT "uuid", "email", "password", "created_at" FROM %s`
+	query := fmt.Sprintf(queryFmt, q.customerTableName)
+
+	var whereClause strings.Builder
+	for key, value := range filter {
+		switch key {
+		case "uuid":
+			if whereClause.Len() != 0 {
+				whereClause.WriteString(" AND ")
+			}
+			whereClause.WriteString(fmt.Sprintf(`"%s"" = %s`, key, value))
+		case "email":
+			if whereClause.Len() != 0 {
+				whereClause.WriteString(" AND ")
+			}
+			whereClause.WriteString(fmt.Sprintf(`"%s"" = %s`, key, value))
+		}
+	}
+
+	if whereClause.Len() != 0 {
+		query = fmt.Sprintf("%s WHERE %s", query, whereClause.String())
+	}
+
+	var customer customerModel
+	if err := q.db.QueryRow(ctx, query).Scan(
+		&customer.UUID,
+		&customer.Email,
+		&customer.Password,
+		&customer.CreatedAt,
+	); err != nil {
+		return nil, dao.ResolveError(err)
+	}
+
+	return customer.MapToReadModel(), nil
+}
