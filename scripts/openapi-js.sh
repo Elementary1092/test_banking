90eee9610a1743b58595730053e54e474929175c
Elementary1092 <pdead6223@gmail.com>
2023-04-15 01:26:25 +0500

    [CLP] Remove unused script

--- scripts/openapi-js.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-readonly service="$1"
-readonly dir="$2"
-
-docker run -t -i -p 8246:8080 -e SWAGGER_JSON=/$(service).yml -v $(pwd)/docs/$(service)/$(service).yml:/$(service).yml swaggerapi/swagger-ui
